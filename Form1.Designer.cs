5302d40ceb02e6be579809f3619b05fcb473dcbf
Atakan Savaş <a.savas@hotmail.com.tr>
2017-11-13 13:26:46 +0300

    first add

---
+++ Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace CreateRows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(25, 121);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(878, 470);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(342, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 59);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(342, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(219, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 608);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Sample Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using Npgsql;
+using System;
+using System.Windows.Forms;
+
+namespace CreateRows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            var table = textBox1.Text;
+
+            richTextBox1.AppendText(Environment.NewLine + " [Table(\"" + table + "\")]");
+            richTextBox1.AppendText(Environment.NewLine + "  public class TabloAdi : BaseModel  {");
+
+            var cString = "#";
+            using (var conn = new NpgsqlConnection(cString))
+            using (var cmd = new NpgsqlCommand())
+            {
+                conn.Open();
+                cmd.Connection = conn;
+
+                cmd.CommandText = "SELECT * FROM information_schema.columns where table_name = '" + table + "'";
+                using (var reader = cmd.ExecuteReader())
+                    while (reader.Read())
+                    {
+                        var tx = "public ";
+                        var clName = reader["column_name"].ToString();
+                        if (clName.Contains("geom"))
+                            continue;
+
+                        if (clName.Contains("created") || clName.Contains("modified") || clName.Contains("created_by")
+                            || clName.Contains("modified_by") || clName.Contains("gid"))
+                            continue;
+
+                        var udtName = reader["udt_name"].ToString();
+
+                        if (udtName.Contains("int"))
+                        {
+                            tx += "int ";
+                        }
+                        else if (udtName.Contains("numeric"))
+                        {
+                            tx += "decimal ";
+                        }
+                        else if (udtName.Contains("varchar") || udtName.Contains("text"))
+                        {
+                            tx += "string ";
+                        }
+                        else if (udtName.Contains("timestamp"))
+                        {
+                            tx += "DateTime ";
+                        }
+                        else if (udtName.Contains("bool") || udtName.Contains("bit"))
+                        {
+                            tx += "bool ";
+                        }
+
+                        tx += clName + " { get; set; }";
+                        richTextBox1.AppendText(Environment.NewLine + tx);
+                    }
+            }
+            richTextBox1.AppendText(Environment.NewLine + " } ");
+        }
+    }
+}
